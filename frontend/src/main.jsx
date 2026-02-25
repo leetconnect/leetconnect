14b25705cc838b909a1697a47d87eaa5868c1418
Achraf Meziouni <amezioun@student.1337.ma>
2026-02-25 06:07:17 +0000

    LP: Footer, logo font and color update, adding About Term Privacy pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import Landing from './pages/Landing.jsx';
 import { Theme } from './components/Theme';
-import { Intro } from './components/Intro';
-import {Features } from './components/Features';
-import {Howitwork } from './components/Howitwork';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Theme>
-        <App />
-        <Intro/>
-        <Features/>
-        <Howitwork/>
+        <App/>
       </Theme>
     </BrowserRouter>
   </React.StrictMode>,
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,11 +1,19 @@
-// import Theme from "@/components/ThemeShowcase";
 import Navbar from "@/components/Navbar";
-
+import { Intro } from '@/components/Intro';
+import { Features } from '@/components/Features';
+import { Howitwork } from '@/components/Howitwork';
+import { Footer } from '@/components/Footer';
+    
 export default function Landing(){
     return (
-        <>
-            {/* <Theme/> */}
-            <Navbar/>       
-            </> 
+        <div className="flex flex-col min-h-screen">
+            <Navbar/>
+            <main className="flex-1">
+                <Intro/>
+                <Features/>
+                <Howitwork/>
+            </main>
+            <Footer/>
+        </div>
     );
 }
