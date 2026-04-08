00eb7cf060d61add87c67e5ddb769f5eab07578a
42-adbouras <wikaz077@gmail.com>
2026-04-08 15:09:08 +0100

    feat: chat frontend (avatar, message props)

---
+++ frontend/src/pages/chat/MessageLayer.tsx
@@ -0,0 +1,50 @@
+import Avatar from "./Avatar";
+
+export interface Message {
+	id: 			number;
+	content: 		string;
+	sender_id: 		number;
+	sender_name: 	string;
+	sender_avatar?:	string;
+	created_at: 	string;
+	is_mine: 		boolean;
+}
+
+interface MessageProp {
+	message: Message;
+}
+
+export default function MessageLayer({message}: MessageProp) {
+	const time = new Date(message.created_at).toLocaleDateString([], {
+		hour: "2-digit",
+		minute: "2-digit"
+	});
+
+	if (message.is_mine) {
+		return (
+			<div className="flex justify-end mb-4">
+				<div className="max-w-md">
+					<div className="bg-primary/90 text-foreground rounded-2xl rounded-br-sm px-4 py-2.5">
+						<p className="text-sm leading-relaxed">{message.content}</p>
+					</div>
+					<p className="text-xs text-muted-foreground mt-1 text-right">
+						{time}
+					</p>
+				</div>
+			</div>
+		);
+	}
+	return (
+		<div className="flex gap-3 mb-4">
+			<Avatar name={message.sender_name} image={message.sender_avatar} />
+			<div className="max-w-mb">
+				<div className="bg-secondary rounded-2xl rounded-bl-sm px-4 py-2.5">
+					<p className="text-sm text-foreground leading-relaxed">
+						{message.content}
+					</p>
+				</div>
+				<p className="text-xs text-muted-foreground mt-1">{time}</p>
+			</div>
+		</div>
+	);
+}
--- frontend/src/pages/chat/Messages.tsx
+++ frontend/src/pages/chat/Messages.tsx
@@ -1,4 +1,48 @@
 // @adbouras
+// import Avatar from "./Avatar";
+import Message from "./MessageLayer";
+
 export default function Messages() {
-    return null;
+	const m1 = {
+		id: 1,
+		content: "Hello from test message",
+		sender_id: 42,
+		sender_name: "Sarah Chen",
+		sender_avatar: "https://i.pravatar.cc/100?img=5",
+		created_at: new Date().toISOString(),
+		is_mine: false,
+	};
+	const m2 = {
+		id: 2,
+		content: "Hello hello",
+		sender_id: 1337,
+		sender_name: "Sarah Chen",
+		sender_avatar: "https://cdn.intra.42.fr/users/09229a11c453f1262c72a2e85f43ea2e/adbouras.jpg",
+		created_at: new Date().toISOString(),
+		is_mine: false,
+	};
+	const m3 = {
+		id: 2,
+		content: "Hello hello",
+		sender_id: 1337,
+		sender_name: "Sarah Chen",
+		created_at: new Date().toISOString(),
+		is_mine: true,
+	};
+	return (
+		<div>
+			{/* <Avatar name="Adham Bouras" image="https://cdn.intra.42.fr/users/09229a11c453f1262c72a2e85f43ea2e/adbouras.jpg"/>
+			<Avatar name="Sarah Chen"/>
+			<Avatar name="Dev Team Alpha"/>
+
+			<Avatar name="Emily Rodriguez" size="sm"/>
+			<Avatar name="Adham Bouras" size="sm" image="https://cdn.intra.42.fr/users/09229a11c453f1262c72a2e85f43ea2e/adbouras.jpg"/> */}
+			<div className="p-10">
+				<Message message={m1} />
+				<Message message={m2} />
+				<Message message={m3} />
+			</div>
+		</div>
+
+	);
 }
