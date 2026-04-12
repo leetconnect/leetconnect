e5db4dc3c02684a49dcad7e509a0342ebb90f284
Ali <abde69436@gmail.com>
2026-04-12 09:25:12 +0100

    change icons from svg to react icons

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -1,5 +1,12 @@
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
+import { HiOutlineArrowRightOnRectangle,
+				 HiOutlineBriefcase,
+				 HiOutlineChartPie,
+				 HiOutlineShieldCheck,
+				 HiOutlineSquares2X2,
+				 HiOutlineUsers } from "react-icons/hi2";
+
 
 const NAV_MAIN = [
   {
@@ -7,15 +14,7 @@ const NAV_MAIN = [
     href: '/admin',
     minRole: 'guest' as const,
     icon: (
-      <svg
-        className="w-5 h-5"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth={1.5}
-        viewBox="0 0 24 24"
-      >
-        <path strokeLinecap="round" strokeLinejoin="round" d="M4 4h6v6H4zM14 4h6v6h-6zM4 14h6v6H4zM14 14h6v6h-6z" />
-      </svg>
+      <HiOutlineSquares2X2 className='w-4 h-4'/>
     ),
   },
   {
@@ -23,15 +22,7 @@ const NAV_MAIN = [
     href: '/admin/users',
     minRole: 'moderator' as const,
     icon: (
-      <svg
-				fill="none"
-				viewBox="0 0 24 24"
-				strokeWidth={1.5}
-				stroke="currentColor"
-				className="w-5 h-5"
-				>
-  				<path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
-			</svg>
+			<HiOutlineUsers className="h-4 w-4" />
 
     ),
   },
@@ -40,12 +31,7 @@ const NAV_MAIN = [
 		href: '/admin/jobs',
 		minRole: 'moderator' as const,
 		icon: (
-			<svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-</svg>
-
-
-	
+			<HiOutlineBriefcase className='h-4 w-4' />
 		),
 	},
   {
@@ -53,10 +39,7 @@ const NAV_MAIN = [
     href: '/admin/roles',
     minRole: 'admin' as const,
     icon: (
-      <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
-</svg>
-
+      <HiOutlineShieldCheck className='h-4 w-4' />
     ),
   },
   {
@@ -64,12 +47,7 @@ const NAV_MAIN = [
     href: '/admin/analytics',
     minRole: 'admin' as const,
     icon: (
-      <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
-  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
-</svg>
-
-
+      <HiOutlineChartPie className='h-5 w-5' />
     ),
   },
 ];
@@ -126,9 +104,7 @@ export const Sidebar = () => {
           onClick={handleLogout}
           className="w-full flex pl-6 items-center gap-3 px-3 py-2 rounded-lg text-sm text-muted-foreground hover:text-destructive hover:bg-destructive/5 transition-colors"
         >
-          <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-          </svg>
+          <HiOutlineArrowRightOnRectangle className='w-5 h-5' />
           Logout
         </button>
     </aside>
