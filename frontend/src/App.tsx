6a6ea160d4ecd371d6c8462097fe7fdb818fd17d
ner-roui <ner-roui@DESKTOP-AAHHVUE.localdomain>
2026-04-25 15:37:46 +0100

    updating app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,34 +6,39 @@ import Terms from './pages/Terms';
 import About from './pages/About';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
-// import Dashboard from './pages/market/Dashboard';
+import Dashboard from './pages/market/Dashboard';
 import Messages from './pages/chat/Messages';
 import Admin from './pages/admin/Admin';
 import Layout from './components/Layout';
 // import Theme from './components/ThemeShowcase';
 // import Navbar from './components/Navbar';
 import { ScrollToTop } from './components/ScrollToTop';
-import ProfilePage from './pages/profile/ProfilePage';
+import { GuestRoute } from './components/GuestRoute';
 
 export default function App() {
   return (
     <>
       <ScrollToTop />
       <Routes>
+        {/* added guest route protection so authenticated users cant access register/login forms  */}
+        <Route element={<GuestRoute />}>
+          <Route path="/auth/sign-in" element={<Login />} />
+          <Route path="/auth/sign-up" element={<Register />} />
+        </Route>
+        
         {/*Public routes*/}
         <Route path="/" element={<Landing />} />
-        {/* <Route path="/auth/sign-in" element={<Login />} /> */}
-        <Route path="/auth/sign-up" element={<Register />} />
+        {/* <Route path="/auth/sign-in" element={<Login />} />
+        <Route path="/auth/sign-up" element={<Register />} /> */}
         <Route path="/about" element={<About />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
 
         {/*authenticated routes (with shared layout)*/}
         <Route element={<Layout />}>
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          <Route path="/chat" element={<Messages />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/messages" element={<Messages />} />
           <Route path="/admin" element={<Admin />} />
-          <Route path="/profile/:username" element={<ProfilePage />} />
         </Route>
       </Routes>
     </>
