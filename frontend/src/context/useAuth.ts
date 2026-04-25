a3b5a8a62524387c1fce24a99acf008d744c05a4
Ali <abde69436@gmail.com>
2026-04-25 19:16:32 +0100

    integrate frontend with backend APIs, handle data fetching and state synchronization

---
+++ frontend/src/context/useAuth.ts
@@ -0,0 +1,9 @@
+import { AuthContextType } from "@/types";
+import { useContext } from "react";
+import { AuthContext }  from '../context/AuthContext'
+
+export const useAuth = (): AuthContextType => {
+	const ctx = useContext(AuthContext);
+	if (!ctx) throw new Error('useAuth must be used inside <AuthProvider>');
+	return ctx;
+}
